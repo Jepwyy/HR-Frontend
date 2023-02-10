4d8aa8998a060f5e704371ae16e9db371e7816dd
Danielzxccc <araojodaniel321@gmail.com>
2023-02-10 14:53:16 +0800

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import axios from "../api/api";
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
 import { UserAuth } from "../context/authContext";
 import { useNavigate } from "react-router-dom";
 
@@ -9,12 +9,15 @@ import { useNavigate } from "react-router-dom";
 import "../style/login.css";
 // images
 import logo from "../assets/images/logo.png";
+import Spinner from "../components/Spinner";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const { setUserData, setToken } = UserAuth();
   const navigate = useNavigate();
+  const passwordRef = useRef(null);
 
   const mutation = useMutation({
     mutationFn: (userdetails) =>
@@ -22,10 +25,10 @@ const Login = () => {
         headers: { "Content-Type": "application/json" },
       }),
     onError: (error) => {
-      console.log(error);
+      console.log(error.response.data.message);
+      setErrorMessage(error.response.data.message);
     },
     onSuccess: (data) => {
-      console.log(data.data.user[0]);
       setUserData(data.data.user[0]);
       setToken(true);
       navigate("/dashboard");
@@ -41,78 +44,80 @@ const Login = () => {
   };
 
   const show = () => {
-    let x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
+    if (passwordRef.current.type === "password") {
+      passwordRef.current.type = "text";
     } else {
-      x.type = "password";
+      passwordRef.current.type = "password";
     }
   };
-
   return (
-    <div className=" md:justify-between md:flex md:items-center  ">
-      <div>
-        <img src={logo} alt="" className="md:h-auto  md:float-left" />
+    <div className='flex flex-col md:flex-row justify-center items-center'>
+      <div className='md:mx-20'>
+        <img src={logo} alt='' className='md:h-auto w-[720px]' />
       </div>
 
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-4xl">
+      <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 md:mx-20'>
+        <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+          <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-4xl'>
             Login
           </h1>
-          <form className="space-y-4 md:space-y-5" onSubmit={handleSubmit}>
-            <div className="">
-              <label className=" block mb-2 text-sm mr-[80%] font-medium text-gray-900">
+          <div className='flex justify-center'>
+            {mutation.isLoading && <Spinner />}
+          </div>
+          <form className='space-y-4 md:space-y-5' onSubmit={handleSubmit}>
+            <div className='text-black'>{errorMessage}</div>
+            <div className=''>
+              <label className=' block mb-2 text-sm mr-[80%] font-medium text-gray-900'>
                 Username
               </label>
               <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   "
-                placeholder="Enter Username"
-                type="text"
+                className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   '
+                placeholder='Enter Username'
+                type='text'
                 autoFocus
-                autoComplete
                 required
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
-            <div className="">
-              <label className=" place-self-start block mb-2 text-sm mr-[80%] font-medium text-gray-900">
+            <div className=''>
+              <label className=' place-self-start block mb-2 text-sm mr-[80%] font-medium text-gray-900'>
                 Password
               </label>
               <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   "
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Enter Password"
+                className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   '
+                type='password'
+                name='password'
+                id='password'
+                ref={passwordRef}
+                placeholder='Enter Password'
                 required
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div class="flex items-center justify-between">
-              <div class="flex items-start">
-                <div class="ml-1 flex items-center h-5">
+            <div className='flex items-center justify-between'>
+              <div className='flex items-start'>
+                <div className='ml-1 flex items-center h-5'>
                   <input
-                    type="checkbox"
-                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
+                    type='checkbox'
+                    className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800'
+                    required=''
                     onClick={show}
                   />
                 </div>
-                <div class="ml-1 text-sm">
-                  <label class="text-gray-500 dark:text-gray-300">
+                <div className='ml-1 text-sm'>
+                  <label className='text-gray-500 dark:text-gray-300'>
                     Show Password
                   </label>
                 </div>
               </div>
               <a
-                href="#"
-                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                href='#'
+                className='text-sm font-medium text-primary-600 hover:underline dark:text-primary-500'
               >
                 Forgot password?
               </a>
             </div>
-            <button className="bg-blue-600 text-white" type="submit">
+            <button className='bg-blue-600 text-white' type='submit'>
               Sign in
             </button>
           </form>
