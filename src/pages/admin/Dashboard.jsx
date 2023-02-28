48f7898a52169730af346401d873b7f09c91ab2a
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-02-28 23:37:00 +0800

    test

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import React from 'react'
+import dp from '../../assets/images/dp.jpg'
 const Dashboard = () => {
-  return <div className="">a</div>;
-};
+  return (
+    <div className='p-4 md:p-12'>
+      <div className='flex justify-between'>
+        <div className='border-4 border-slate-900 w-52 h-20'>
+          <img className='w-52 h-[4.5rem]' src={dp} />
+        </div>
+        <div className='border-4 border-slate-900 w-52 h-20'>
+          <img className='w-52 h-[4.5rem]' src={dp} />
+        </div>
+        <div className='border-4 border-slate-900 w-52 h-20'>
+          <img className='w-52 h-[4.5rem]' src={dp} />
+        </div>
+        <div className='border-4 border-slate-900 w-52 h-20'>
+          <img className='w-52 h-[4.5rem]' src={dp} />
+        </div>
+      </div>
+      <div className='border-4 border-slate-900 w-full h-96 mt-16'>
+        <img className='w-full h-full' src={dp} />
+      </div>
+    </div>
+  )
+}
 
-export default Dashboard;
+export default Dashboard
