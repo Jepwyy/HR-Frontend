577006532014a144d398d980638345e9515af4cd
Danielzxccc <araojodaniel321@gmail.com>
2023-04-26 23:51:40 +0800

    dashboard dynamic chart data

--- src/components/Dashboard/DoughnutChart.jsx
+++ src/components/Dashboard/DoughnutChart.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 import { Doughnut } from 'react-chartjs-2'
+import { useQuery } from 'react-query'
+import axios from '../../api/api'
 
-const DoughnutChart = ({ pieData, chartOptions }) => {
+const DoughnutChart = ({ chartOptions }) => {
+  const { data, isLoading, isError } = useQuery('barchartData', () =>
+    axios.get('/report/piechart').then((res) => res.data)
+  )
+  let chartData
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error...</div>
+  if (data)
+    chartData = {
+      labels: Object.keys(data),
+      datasets: [
+        {
+          label: 'Employee Count',
+          data: Object.values(data),
+          backgroundColor: ['#ac7238', '#1d0e0b', '#d88c51', '#f3e7c9'],
+          borderColor: '#010100',
+          borderWidth: 2,
+        },
+      ],
+    }
+
   return (
     <motion.span
       animate={{ opacity: 1 }}
@@ -14,7 +36,10 @@ const DoughnutChart = ({ pieData, chartOptions }) => {
         Total Employees
       </h1>
       <div className='h-[85%]'>
-        <Doughnut data={pieData} options={chartOptions} />
+        <Doughnut
+          data={chartData}
+          options={chartOptions}
+        />
       </div>
     </motion.span>
   )
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -14,47 +14,21 @@ import LineChart from '../../components/Dashboard/LineChart'
 import { SalaryData, UserData, PieData } from '../../utils/tempdata'
 
 import { Chart, registerables } from 'chart.js'
+import { useQuery } from 'react-query'
+import axios from '../../api/api'
+
 Chart.register(...registerables)
 
 const Dashboard = () => {
-  const [active, setActive] = useState('FirstChart')
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.month),
-    datasets: [
-      {
-        label: 'Employee Attendance',
-        data: UserData.map((data) => data.Attendance),
-        backgroundColor: ['#ac7238'],
-        borderColor: '#462e16',
-        borderWidth: 2,
-      },
-    ],
-  })
+  const { data, isLoading, isError } = useQuery(['summaryData'], () =>
+    axios.get('report/summary').then((res) => res.data)
+  )
 
-  const [salaryData, setSalaryData] = useState({
-    labels: SalaryData.map((data) => data.month),
-    datasets: [
-      {
-        label: 'Employee Salary',
-        data: SalaryData.map((data) => data.salary),
-        backgroundColor: ['#ac7238'],
-        borderColor: '#462e16',
-        borderWidth: 2,
-      },
-    ],
-  })
-  const [pieData, setPieData] = useState({
-    labels: PieData.map((data) => data.department),
-    datasets: [
-      {
-        label: 'Employee Count',
-        data: PieData.map((data) => data.count),
-        backgroundColor: ['#ac7238', '#1d0e0b', '#d88c51', '#f3e7c9'],
-        borderColor: '#010100',
-        borderWidth: 2,
-      },
-    ],
-  })
+  const [active, setActive] = useState('FirstChart')
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error...</div>
+
   const chartOptions = {
     responsive: true,
     maintainAspectRatio: false,
@@ -68,52 +42,52 @@ const Dashboard = () => {
     <div className='p-4 md:p-12'>
       <div className='flex bg-[#F5F5F5]'>
         <div className='w-full md:h-[30rem] h-[37.5rem] p-5'>
-          {active === 'FirstChart' && (
-            <BarChart userData={userData} chartOptions={chartOptions} />
-          )}
+          {active === 'FirstChart' && <BarChart chartOptions={chartOptions} />}
           {active === 'SecondChart' && (
-            <DoughnutChart pieData={pieData} chartOptions={chartOptions} />
+            <DoughnutChart chartOptions={chartOptions} />
           )}
-          {active === 'ThirdChart' && (
-            <LineChart salaryData={salaryData} chartOptions={chartOptions} />
-          )}
+          {active === 'ThirdChart' && <LineChart chartOptions={chartOptions} />}
         </div>
       </div>
       <div className='flex md:flex-row flex-col gap-3 justify-between md:mt-8 mt-5 md:mb-0 mb-20'>
         <div
           onClick={() => setActive('FirstChart')}
-          className='md:w-60 w-full h-24 bg-[#F5F5F5] rounded flex select-none'
+          className='md:w-60 w-full h-24 bg-[#F5F5F5] rounded flex select-none cursor-pointer'
         >
           <div className='w-[40%] flex justify-center  items-center'>
             <GoCalendar size={50} />
           </div>
-          <div className='w-[80%] flex justify-center flex-col '>
-            <h1 className='font-bold text-4xl text-[#AC7238] '>100</h1>
+          <div className='w-[80%] flex justify-center flex-col'>
+            <h1 className='font-bold text-4xl text-[#AC7238] '>
+              {data.attendance[0].count}
+            </h1>
             <h1 className='font-bold text-gray-400'>PRESENT TODAY</h1>
           </div>
         </div>
         <div
           onClick={() => setActive('SecondChart')}
-          className='md:w-60 w-full h-24 bg-[#F5F5F5] rounded flex select-none'
+          className='md:w-60 w-full h-24 bg-[#F5F5F5] rounded flex select-none cursor-pointer'
         >
           <div className='w-[40%] flex justify-center  items-center'>
             <ImUsers size={50} />
           </div>
           <div className='w-[80%] flex justify-center flex-col '>
-            <h1 className='font-bold text-4xl text-[#AC7238] '>100</h1>
+            <h1 className='font-bold text-4xl text-[#AC7238] '>
+              {data.totalEmployees[0].count}
+            </h1>
             <h1 className='font-bold text-gray-400'>TOTAL EMPLOYEES</h1>
           </div>
         </div>
         <div
           onClick={() => setActive('ThirdChart')}
-          className='md:w-60 w-full h-24 bg-[#F5F5F5] rounded flex select-none'
+          className='md:w-60 w-full h-24 bg-[#F5F5F5] rounded flex select-none cursor-pointer'
         >
           <div className='w-[40%] flex justify-center  items-center'>
             <GiTakeMyMoney size={50} />
           </div>
           <div className='w-[80%] flex justify-center flex-col '>
             <h1 className='font-bold text-4xl text-[#AC7238] w-36 text-shrink overflow-hidden whitespace-nowrap text-ellipsis'>
-              12321
+              {data.totalPayout[0].sum}
             </h1>
             <h1 className='font-bold text-gray-400'>TOTAL PAYOUTS</h1>
           </div>
